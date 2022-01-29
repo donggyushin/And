89744f1e4aad3786deef8445c19d094bb478abe9
신동규 <donggyu9410@gmail.com>
2022-01-30 02:20:50 +0900

    Write and function

--- Sources/And/And.swift
+++ Sources/And/And.swift
@@ -1,6 +1,21 @@
-public struct And {
-    public private(set) var text = "Hello, World!"
+import UIKit
 
-    public init() {
+public protocol And {}
+
+extension And where Self: AnyObject {
+    /// Makes it available to set properties with closures just after initializing.
+    /// @inlinable is a function that informs the compiler of the inside of the function as part of the module's interface for optimization.
+    @inlinable
+    public func and(_ block: (Self) -> Void) -> Self {
+        block(self)
+        return self
     }
 }
+
+extension NSObject: And {}
+extension Array: And {}
+extension Dictionary: And {}
+extension Set: And {}
+extension UIEdgeInsets: And {}
+extension UIOffset: And {}
+extension UIRectEdge: And {}
